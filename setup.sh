e232b1157de311f51dc82e56205ce7f45118cbaa
Fivevisor Manager <fivevisor@gmail.com>
2025-03-22 10:16:43 +0300

    Update

---
+++ setup.sh
@@ -0,0 +1,39 @@
+# General Update
+sudo apt update && sudo apt upgrade -y
+
+# Docker Setup
+sudo apt install -y docker.io
+sudo systemctl enable --now docker
+
+# Docker Compose Setup
+sudo apt install -y docker-compose
+
+# Generating SSH Key
+echo "Enter your e-mail address:"
+read EMAIL_ADDRESS
+ssh-keygen -t ed25519 -C $EMAIL_ADDRESS -f ~/.ssh/id_ed25519 -N ""
+
+# Adding SSH Key to GitHub
+echo "Enter your GitHub username:"
+read GITHUB_USER
+echo "Enter your GitHub personal access token:"
+read GITHUB_TOKEN
+SSH_KEY=$(cat ~/.ssh/id_ed25519.pub)
+curl -u "$GITHUB_USER:$GITHUB_TOKEN" --data '{"title":"Ubuntu Server Key","key":"'$SSH_KEY'"}' https://api.github.com/user/keys
+
+# Trigger GitHub Workflows
+echo "Enter your GitHub organisation name:"
+read GITHUB_ORG
+echo "Enter repositorys separated by a space:"
+read REPOS
+for REPO in "${REPOS[@]}"; do
+  curl -X POST -u "$GITHUB_USER:$GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/$GITHUB_ORG/$REPO/actions/workflows/main.yml/dispatches" \
+    -d '{"ref":"main"}'
+done
+
+echo "Setup completed."
+
+# Use the following command to easily run.
+# bash <(curl -sL https://raw.githubusercontent.com/fivevisor/devops-config/main/setup.sh)
